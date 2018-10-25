46a6fdca19d7fb0a34c2115228317cd3491f6323
hellenfa <hellenfatima1505@gmail.com>
2018-10-25 13:42:57 +0700

    file baru

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -79,11 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Belajar Laravel
+                    About
                 </div>
+
                 <div class="links">
-                  <a href="pesan">Pesan</a>
-                  <a href="about">About</a>
+                    <p>Halaman web ini</p><br/>
+                    <a href="/">Home</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//nama routing halaman about -> About
+//ketika mengambil about, akan mengembalikan nilai view
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('pesan', function () { //di welcome routingnya pesan
+    return view('pesan'); //memanggil pesan.blade.php
+});
+
+Route::post('pesan/kirim', function () { //method post
+    return view('pesan-kirim');
+});
